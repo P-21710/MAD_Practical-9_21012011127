cb4848b9d905fc6ff80921276e2ac71f661666ca
P-21710 <98374171+P-21710@users.noreply.github.com>
2023-10-03 08:54:28 +0530

    Practical-9

---
+++ app/src/main/java/com/prasadrajyaguru/mad_practical_9_21012011127/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.prasadrajyaguru.mad_practical_9_21012011127
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var alarmanimation : AnimationDrawable
+    lateinit var heartanimation: AnimationDrawable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val heart:ImageView=findViewById(R.id.image_heart)
+        heart.setBackgroundResource(R.drawable.heart_animation_list)
+        heartanimation=heart.background as AnimationDrawable
+
+        val alarm : ImageView = findViewById(R.id.image_alarm)
+        alarm.setBackgroundResource(R.drawable.alarm_animation_list)
+        alarmanimation = alarm.background as AnimationDrawable
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            alarmanimation.start()
+            heartanimation.start()
+        }
+        else {
+            alarmanimation.stop()
+            heartanimation.stop()
+        }
+    }
+}
