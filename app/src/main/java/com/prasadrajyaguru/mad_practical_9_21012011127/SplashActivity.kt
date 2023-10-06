9d0d9bb2c6025af63666688b0bab6bad2dfe7408
P-21710 <98374171+P-21710@users.noreply.github.com>
2023-10-06 08:59:49 +0530

    Practical-9

--- app/src/main/java/com/prasadrajyaguru/mad_practical_9_21012011127/SplashActivity.kt
+++ app/src/main/java/com/prasadrajyaguru/mad_practical_9_21012011127/SplashActivity.kt
@@ -2,10 +2,23 @@ package com.prasadrajyaguru.mad_practical_9_21012011127
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var logoimage : ImageView
+    lateinit var logoanimation : Animation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        logoimage=findViewById<ImageView>(R.id.logo)
+
+        logoanimation=AnimationUtils.loadAnimation(this,R.anim.twin_animation)
+        logoanimation.setAnimationListener(this)
     }
-}
+},
+        animation.animationlistener
